45857b48f9477625169a8e067743a778d2c975e2
Paulo DAlberti <phdalberti@gmail.com>
2019-06-04 14:01:03 +0900

    dependencies

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :exam
-  has_many :answers
+  has_many :answers, dependent: :destroy
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,5 @@
 class Submission < ApplicationRecord
   belongs_to :exam
+  has_many :answers, dependent: :destroy
   mount_uploader :image, ImageUploader
 end

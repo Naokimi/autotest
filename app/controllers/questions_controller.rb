3fdfcdaa5ba0117a4847b03c8717fce6d18e76dc
Rei Ishinabe <rishinabe@gmail.com>
2019-06-04 15:17:21 +0900

    added questions index7
    q
    exit
    c

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,10 @@
 class QuestionsController < ApplicationController
+  def index
+    @exam = Exam.find(params[:exam_id])
+    @questions = policy_scope(Question)
+  end
+
+
   def show
     @question = Question.find(params[:id])
     @answers = @questions.answers

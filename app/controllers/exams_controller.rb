bc47ea29475fa94e503528298b7089ca6056d1b0
Rei Ishinabe <rishinabe@gmail.com>
2019-06-04 16:20:17 +0900

    added a feature to upload new exam

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -2,4 +2,21 @@ class ExamsController < ApplicationController
   def index
     @exams = policy_scope(Exam)
   end
+
+  def create
+    @exam = Exam.new(exam_params)
+    @exam.teacher_id = current_teacher.id
+    authorize @exam
+    if @exam.save
+      redirect_to exams_path
+    else
+      render root_path
+    end
+  end
+
+  private
+
+  def exam_params
+    params.require(:exam).permit(:image)
+  end
 end

1ed3e9c78b52756fd0adfa4e2902e1e10ec522a2
Cesar Murillo <namiayumis@gmail.com>
2019-06-10 19:45:47 +0900

    minimagic_export_pdf

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,6 +2,8 @@ class Exam < ApplicationRecord
   belongs_to :teacher
   has_many :questions, dependent: :destroy
   has_many :submissions, dependent: :destroy
+  has_many :answers, through: :submissions
+
   mount_uploader :image, ImageUploader
 
   validates :image, :teacher_id, presence: true

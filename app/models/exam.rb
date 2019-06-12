451d9d837fb91c7cf10485048aae9ee64a894a84
Rei Ishinabe <rishinabe@gmail.com>
2019-06-12 18:35:41 +0900

    updated submissions-index

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -7,4 +7,42 @@ class Exam < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   validates :image, :teacher_id, presence: true
+
+  def average_score
+    return 0 if submissions.empty?
+
+    sum = 0.0
+    submissions.each do |submission|
+      sum += submission.score
+    end
+    return (sum / submissions.count).floor(1)
+  end
+
+  def average_percentage
+    return 0 if submissions.empty?
+
+    (average_score / questions.count * 100).round
+  end
+
+  def highest_score
+    return 0 if submissions.empty?
+
+    scores = []
+    submissions.each do |submission|
+      scores << submission.score
+    end
+    scores.max
+  end
+
+  def lowest_score
+    return 0 if submissions.empty?
+
+    scores = []
+    submissions.each do |submission|
+      scores << submission.score
+    end
+    scores.min
+  end
+
+
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,4 +2,8 @@ class Submission < ApplicationRecord
   belongs_to :exam
   has_many :answers, dependent: :destroy
   mount_uploader :image, ImageUploader
+
+  def score
+    self.answers.where('is_correct = true').count
+  end
 end

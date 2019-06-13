d5b8e596b5c41589c33a28bdabeb18145f7984e8
Rei Ishinabe <rishinabe@gmail.com>
2019-06-13 14:00:12 +0900

    added a style for uploading

--- app/javascript/packs/upload_button.js
+++ app/javascript/packs/upload_button.js
@@ -7,6 +7,6 @@ if (upload) {
   upload.addEventListener("change", (event) => {
     submit.click();
     console.log("display loading...");
-    body.insertAdjacentHTML('afterbegin', "<div style='position: fixed; z-index:1; width:100%; height:100%; text-align:center; background-color: rgba(255, 255, 255, 0.7)'><%= image_tag 'Spinner-1.4s-200px.gif', class:'mt-5 pt-5' %><h2 class='mt-5 text-primary'>Uploading images...please wait</h2></div>");
+    body.insertAdjacentHTML('afterbegin', "<div class='uploading'><h2 class='text-primary'>Uploading images...please wait</h2></div>");
   });
 }

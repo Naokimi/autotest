4038cae07ad47543f35dd5ed856dbcc662c44ca4
Rei Ishinabe <rishinabe@gmail.com>
2019-06-13 11:29:33 +0900

    added uploading image view

--- app/javascript/packs/upload_button.js
+++ app/javascript/packs/upload_button.js
@@ -3,8 +3,10 @@ const upload = document.getElementById("upload");
 
 if (upload) {
   const submit = document.getElementById("submit");
+  const body = document.querySelector("body");
   upload.addEventListener("change", (event) => {
-    console.log("display loading...");
     submit.click();
+    console.log("display loading...");
+    body.insertAdjacentHTML('afterbegin', "<div style='position: fixed; z-index:1; width:100%; height:100%; text-align:center; background-color: rgba(255, 255, 255, 0.7)'><%= image_tag 'Spinner-1.4s-200px.gif', class:'mt-5 pt-5' %><h2 class='mt-5 text-primary'>Uploading images...please wait</h2></div>");
   });
 }
